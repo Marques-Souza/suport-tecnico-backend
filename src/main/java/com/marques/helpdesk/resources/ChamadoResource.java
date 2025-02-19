f0c3963767c4693737709331ac5d69d3ec013dd9
Marques-Souza <marques.wsouza357@gmail.com>
2025-02-19 14:20:54 -0300

    Endpoint create chamado implementado

--- src/main/java/com/marques/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/marques/helpdesk/resources/ChamadoResource.java
@@ -5,12 +5,11 @@ import com.marques.helpdesk.domain.dtos.ChamadoDTO;
 import com.marques.helpdesk.services.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,4 +32,12 @@ public class ChamadoResource {
         List<ChamadoDTO> listDto = lis.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
     }
+
+    @PostMapping
+    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDto) {
+        Chamado obj = service.create(objDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
